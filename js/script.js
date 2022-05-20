c3aca51c31e047723e66d29b57a92b992f6cbb98
けだま <68723484+Kedama-Neya@users.noreply.github.com>
2022-05-20 09:47:34 +0900

    Update script.js
    無駄な処理ループを削除しました。

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
-window.addEventListener('load', main, false);
+const InitCheck = setInterval(Loaded, 1000);
 
-function main() {
-  const InitCheck = setInterval(Loaded, 1000);
-  function Loaded() {
-    var descriptionElement = document.querySelector('ytd-expander.style-scope.ytd-video-secondary-info-renderer');
-    var moreButton = document.querySelector('yt-formatted-string.more-button.style-scope.ytd-video-secondary-info-renderer');
-    if (descriptionElement != null) {
-      descriptionElement.removeAttribute('style');
-    };
-    if (moreButton != null) {
-      moreButton.remove();
-    };
+function Loaded() {
+  var descriptionElement = document.querySelector('ytd-expander.style-scope.ytd-video-secondary-info-renderer');
+  var moreButton = document.querySelector('yt-formatted-string.more-button.style-scope.ytd-video-secondary-info-renderer');
+  if (descriptionElement != null) {
+    descriptionElement.removeAttribute('style');
+    var dEFlag = 1;
+    console.log("[YTDExtender] 概要欄を展開しました。")
   };
+  if (moreButton != null) {
+    moreButton.remove();
+    var mBFlag = 1;
+    console.log("[YTDExtender] もっと見るボタンを削除しました。")
+  };
+  if (dEFlag == 1 && mBFlag == 1) {
+    clearInterval(InitCheck);
+    console.log("[YTDExtender] 処理を完了しました。")
+  }
 };
